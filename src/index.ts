ed58ffa21a698ffdb3857bb13356a3963a979fd2
Pusit.S <pusitclb@gmail.com>
2025-02-08 13:22:46 +0700

    task 2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, TypeScript with Node.js!");
+let x:number | string = 10;
+x = 2;
+
+if (typeof x === 'string') {
+  console.log('x is a string');
+}else if (typeof x === 'number') {
+  console.log('x is a number');
+}else {
+  console.log('x is neither a string or number');
+}
