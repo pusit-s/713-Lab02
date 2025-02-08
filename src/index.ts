dd14acb871c76bfa3424e753134b47d0c823b841
Pusit.S <pusitclb@gmail.com>
2025-02-08 13:43:23 +0700

    Revert "task 2: add number"
    This reverts commit fd9f59d8e9fb6f0a959f58a34e1319acc2760309.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-const add = (a: number, b: number):string => {
-    const result = a + b;
-    return result.toString();
+let x:number | string = 10;
+x = 2;
+
+if (typeof x === 'string') {
+  console.log('x is a string');
+}else if (typeof x === 'number') {
+  console.log('x is a number');
+}else {
+  console.log('x is neither a string or number');
 }
-const result = add(1,2) + 0;
-console.log(result, 'type of result:', typeof result);
