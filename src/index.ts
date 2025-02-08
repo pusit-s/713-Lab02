fd9f59d8e9fb6f0a959f58a34e1319acc2760309
Pusit.S <pusitclb@gmail.com>
2025-02-08 13:38:05 +0700

    task 2: add number

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-let x:number | string = 10;
-x = 2;
-
-if (typeof x === 'string') {
-  console.log('x is a string');
-}else if (typeof x === 'number') {
-  console.log('x is a number');
-}else {
-  console.log('x is neither a string or number');
+const add = (a: number, b: number):string => {
+    const result = a + b;
+    return result.toString();
 }
+const result = add(1,2) + 0;
+console.log(result, 'type of result:', typeof result);
