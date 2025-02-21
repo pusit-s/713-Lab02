8ebe825d5b664816b2b963b39e9af45fba3053aa
Pusit.S <pusitclb@gmail.com>
2025-02-21 15:21:25 +0700

    task 11: url output endpoint

--- src/services/UploadFileService.ts
+++ src/services/UploadFileService.ts
@@ -1,7 +1,7 @@
 import s3Client from "../awsConfig";
 import { PutObjectAclCommand } from "@aws-sdk/client-s3";
 
-export async function uploadFile(bucket: string, filePath: string, file: Express.Multer.File): Promise<void> {
+export async function uploadFile(bucket: string, filePath: string, file: Express.Multer.File): Promise<string> {
 
     const params = {
         Bucket: bucket,
@@ -13,6 +13,9 @@ export async function uploadFile(bucket: string, filePath: string, file: Express
     try {
         const data = await s3Client.send(new PutObjectAclCommand(params));
         console.log("Successfully uploaded file", data);
+        const publicUrl = `https://jevhdzsjeoykeblpcyph.supabase.co/storage/v1/object/public/images/${filePath}`;
+        console.log("Public URL", publicUrl);
+        return publicUrl;
     } catch (error) {
         console.error("Error uploading file", error);
         throw error;
