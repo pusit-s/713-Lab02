65f3332e66737b262db576ca1bf9a58fd3aa0518
Pusit.S <pusitclb@gmail.com>
2025-02-21 21:21:45 +0700

    task 13: refactor route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
-import { addEvent, getAllEvents, getEventByCategory, getEventById } from "./services/EventService";
-import { Event } from "./models/Event";
 import multer from 'multer';
 import { uploadFile } from './services/UploadFileService';
 import dotenv from 'dotenv';
+import eventRoute from './routes/EventRoute';
 dotenv.config();
 
 const app = express();
+app.use(express.json())
+app.use('/events', eventRoute);
+
 const port = 3000;
 const upload = multer({storage: multer.memoryStorage()});
 const bucket = process.env.SUPABASE_BUCKET_NAME;
@@ -15,8 +17,6 @@ const filePath = process.env.UPLOAD_DIR;
 if (!bucket || !filePath) {
     throw new Error("Bucket name or file path not provided");
 }
-
-app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
@@ -30,32 +30,6 @@ app.get('/test', (req: Request, res: Response) => {
     const id = req.query.id;
     const output = `id: ${id}`;
     res.send(output);
-});
-
-app.get('/events', async(req: Request, res: Response) => {
-    if (req.query.category) {
-        const category = req.query.category as string;
-        const filteredEvents = await getEventByCategory(category as string);
-        res.json(filteredEvents);
-    } else {
-        res.json(await getAllEvents());
-    }
-});
-
-app.get('/events/:id', async(req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    const event = await getEventById(id);
-    if (event) {
-        res.json(event);
-    } else {
-        res.status(404).send('Event not found');
-    }
-});
-
-app.post('/events', async(req: Request, res: Response) => {
-    const newEvent: Event = req.body;
-    await addEvent(newEvent)
-    res.json(newEvent)
 });
 
 app.post('/upload', upload.single('file'), async(req: Request, res: Response) => {
